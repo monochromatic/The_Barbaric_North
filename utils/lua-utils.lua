b54290d72bd7291f3314e1962eef5c025dd3aa35
Andrew Hsu <elvish.sovereign@gmail.com>
2011-04-07 23:26:15 -0400

    Fixed some errors and began coding the 5th scenario. Fixed the {TBN_DEATHS} bug.

--- utils/lua-utils.lua
+++ utils/lua-utils.lua
@@ -22,7 +22,7 @@ function wml_actions.limit_recruits(cfg)
 end
 
 function wml_actions.frostbite_event(cfg)
-	if fire_event then
+	if cfg.fire_event then
 		local current_side = wesnoth.get_variable("side_number")
 		local frostbite_units = wesnoth.get_units { side = current_side , { "not" , { id = "Buggles" }} , { "filter_location" , { terrain = "A*,*^F*a,Ms,Ha" , { "not" , { terrain = "*^V*" }}}}}
 		for index, unit in ipairs(frostbite_units) do
@@ -56,4 +56,25 @@ function wesnoth.theme_items.unit_status()
 		} })
 	end
 	return s
+end
+
+function wml_actions.spawn_units(cfg)
+	if cfg.fire_event then
+		local side_two = wesnoth.get_units { side = 2 }
+		local units_placed = #side_two
+		local possible_locs = wesnoth.get_locations { x = 31-38 , y = 1-7 }
+		local current_turn = wesnoth.get_variable("turn_number")
+		
+		if current_turn < 10 then local number_of_units = 15
+		elseif current_turn < 20 then local number_of_units = 20
+		elseif current_turn >= 21 then local number_of_units = 30
+		end
+		
+		while units_placed <= number_of_units do
+			local rand_index = math.random(1, #possible_locs)
+			wesnoth.put_unit(possible_locs[rand_index][1], possible_locs[rand_index][2], helper.rand("Bowman,Javelineer,Longbowman,Mage,Pikeman,Red Mage,Spearman,Spearman,Swordsman,Swordsman"))
+			table.remove(possible_locs, rand_index)
+			units_placed = units_placed + 1
+		end
+	end
 end
